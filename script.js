d31c634d6a38305a582853d4544a460be0bbee61
SophieeV <sophievesin@wanadoo.fr>
2021-04-29 16:53:05 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#green").click(function(){
+        $("#texte").css("color","green");
+      });
+  $("#red").click(function(){
+        $("#texte").css("color","red");
+      });
+  $("#blue").click(function(){
+        $("#texte").css("color","blue");
+      });
+  });
